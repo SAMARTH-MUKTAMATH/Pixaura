d31d81dfb9ac6245b9a8f443879e1d92a3ef8e6f
SAMARTH MUKTAMATH <samarthmise2025@gmail.com>
2025-10-29 22:35:09 +0530

    feat: added logo to footer component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,14 +3,14 @@ import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
-    <div className='flex items-center justify-between gap-4 py-3 mt-20'>
+    <div className='flex items-center justify-between  gap-4 py-3 mt-20'>
        <div className="flex items-center gap-2 group">
          <img 
            src={assets.favicon} 
            alt="Pixaura" 
-           className='w-16 sm:w-20 lg:w-24 transition-transform duration-700 ease-in-out group-hover:rotate-[360deg] transform-gpu'
+           className='w-14 sm:w-16 md:w-20 lg:w-24 transition-transform duration-700 ease-in-out group-hover:rotate-[360deg] transform-gpu'
          />
-         <h1 className="text-xl sm:text-2xl lg:text-3xl font-bold bg-gradient-to-r from-blue-600 via-purple-600 to-blue-800 bg-clip-text text-transparent tracking-tight">
+         <h1 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold text-gray-500 tracking-tight">
            Pixaura
          </h1>
        </div>
